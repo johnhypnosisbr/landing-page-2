800680553c744e3051f2bb4d1560f1cf76b99c3d
johnhypnosisbr <john.kmick@yahoo.com.br>
2023-08-20 20:49:41 -0300

    new logo

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,19 +1,9 @@
 export default function Header() {
   return (
-    <header className="absolute w-full z-30">
-      <div className="max-w-6xl mx-auto px-4 sm:px-6 flex flex-row">
-          {/* Site branding */}
-          <div className="mr-4">
-            {/* Logo */}
+    <header className="absolute w-full z-30 flex align-center justify-center">
             <a href="https://business-card-mu-virid.vercel.app/" className="" aria-label="Logo Link">
-              <div className="flex flex-row justify-center align-center">
-              <img src="./logo.png" alt="" className="logo-img " />
-                <h1 className="pt-8">John Joseph | Web Dev</h1>
-              </div>
+              <img src="./logoNew.png" alt="" className="logo-img " />
             </a>
-          </div>
-    
-      </div>
     </header>
   )
 }
